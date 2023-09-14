39d30c59c59914e130a7fd4ed10a5bc4fc8c85e9
sebbehakansson <sebastian.sh74@gmail.comm>
2023-09-14 09:07:53 +0200

    some endpionts

--- src/server/main.ts
+++ src/server/main.ts
@@ -4,6 +4,7 @@ import express, {Request, Response} from "express";
 import ViteExpress from "vite-express";
 import mongoose from 'mongoose';
 import User from "./models/User";
+import Product from "./models/Product"
 
 const app = express();
 
@@ -24,6 +25,36 @@ app.post('/user', async (req: Request, res: Response) => {
   } catch (error) {
     console.error('Error creating user:', error);
     res.status(500).json({ error: 'An error occurred while creating the user' });
+  }
+});
+
+app.get('/products', async (req: Request, res: Response) => {
+  try {
+    const products = await Product.find();
+    console.log('products', products);
+    
+    // Send the products as a JSON response
+    res.json(products);
+  } catch (error) {
+    console.error('Error fetching products:', error);
+    // Handle the error and send an appropriate response
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.post('/product', async (req: Request, res: Response) => {
+  try {
+    const newProduct = new Product({
+    productName: 'fan',
+    productPrice: 100,
+    size: 'S',
+    });
+    const createdProduct = await newProduct.save();
+    console.log('Product created:', createdProduct);
+    res.json(createdProduct);
+  } catch (error) {
+    console.error('Error creating product:', error);
+    res.status(500).json({ error: 'An error occurred while creating the product' });
   }
 });
 
