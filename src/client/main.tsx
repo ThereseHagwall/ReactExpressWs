6e5dcff44a1840cda2e59ea783d175f7e56cd993
Felix Anneblad <felix.anneblad@gmail.com>
2023-09-18 15:38:08 +0200

    Created new comp for single prod.

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -3,12 +3,15 @@ import "./index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import ErrorBoundary from "./components/ErrorBound";
+import { BrowserRouter } from 'react-router-dom'
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <ErrorBoundary fallback="Has error.">
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ErrorBoundary>
   </React.StrictMode>
 );
