6e0147722a55568b75b1932547d4876f7cfdb95c
Felix Anneblad <felix.anneblad@gmail.com>
2023-09-18 10:15:05 +0200

    Fetch and display products in FE.

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,9 +1,11 @@
+import FetchProducts from "./components/FetchProducts";
 
 function App() {
 
   return (
     <>
-    <div><h1>Webbshop</h1></div>
+      <div><h1>Webbshop</h1></div>
+      <FetchProducts />
     </>
   );
 }
