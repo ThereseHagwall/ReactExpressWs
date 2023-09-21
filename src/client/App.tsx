9d4285faa1037bba57e6b428bc1746c2a37f080d
Matias Bengtsson <bengtsson1985@hotmail.com>
2023-09-21 10:15:25 +0200

    checkout Endpoint

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -4,6 +4,7 @@ import MainContent from "./components/MainContent";
 import PageNotFound from "./components/PageNotFound";
 import FetchSingleProduct from "./components/FetchSingleProduct";
 import Header from "./components/Header";
+import CheckoutCart from "./components/CheckoutCart";
 import LoginForm from "./components/LoginForm";
 import AdminView from "./components/AdminView";
 
@@ -16,12 +17,11 @@ function App() {
     <>
       <Header />
       <Routes>
-        <Route path="*" element={<PageNotFound />} />
-        <Route path="/" element={<MainContent />} />
-        <Route path="/orderlist" element={<OrderList />} />
-        <Route path="/products/:productId" element={<ProductPage />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/admin" element={<AdminView />} />
+        <Route path='*' element={<PageNotFound />} />
+        <Route path='/' element={<MainContent />} />
+        <Route path='/orderlist' element={<OrderList />} />
+        <Route path='/products/:productId' element={<ProductPage />} />
+        <Route path='/checkoutcart' element={<CheckoutCart />} />
       </Routes>
     </>
   );
